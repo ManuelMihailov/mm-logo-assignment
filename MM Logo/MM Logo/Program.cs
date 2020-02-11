745b654d2ba2dc2a7dab319ad8cc847b8910710b
Manuel Mihailov <blanksenpai123@gmail.com>
2020-02-11 19:04:24 +0200

    Created solution.

---
+++ MM Logo/MM Logo/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace MM_Logo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int mid;
+            int top;
+            string half;
+
+            int n = int.Parse(Console.ReadLine());
+            int bottom = 1;
+            int bottomMid = n * 2 - 1;
+
+            for (int i = 0; i <= n; i++)
+            {
+                if (i + 1 <= (n + 1) / 2)
+                {
+                    mid = n - (2 * i);
+                    top = n + (i * 2);
+                    half = $"{Print('-', n - i)}{Print('*', top)}{Print('-', mid <= 0 ? 0 : mid)}{Print('*', top)}{Print('-', n - i)}";
+                    Console.WriteLine($"{half}{half}");
+                }
+                else
+                {
+                    half = $"{Print('-', n - i)}{Print('*', n)}{Print('-', bottom)}{Print('*', bottomMid)}{Print('-', bottom)}{Print('*', n)}{Print('-', n - i)}";
+                    Console.WriteLine($"{half}{half}");
+                    bottom += 2;
+                    bottomMid -= 2;
+                }
+            }
+        }
+
+        static string Print(char symbol, int count)
+        {
+            return new string(symbol, count);
+        }
+    }
+}
